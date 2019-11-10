377994cbd0fac7ae1d91d5e87b1a5fbb2092b2ff
Turza006 <probaldattaturza@gmail.com>
2019-11-10 22:36:33 +0600

    models update

---
+++ score/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Players
+
+
+class PlayersSerializer(ModelSerializer):
+    class Meta:
+        model = Players
+        fields = '__all__'
